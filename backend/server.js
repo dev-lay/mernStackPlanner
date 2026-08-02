ae54bfe430e2342fe90618467090b95c171266b4
Layana Hatem <200230937+dev-lay@users.noreply.github.com>
2026-08-02 19:28:11 +0300

    Add API endpoint for health check

--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,8 @@ connectDB().then(() => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 app.use(express.json());
 app.use("/api/tasks", routeTask);
